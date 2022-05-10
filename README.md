9932da40b09db877fab10a263c12b7bd05e3abb9
vijay ankith <vijay2ankith@gmail.com>
2022-05-10 21:58:37 +0530

    added enums structs mappings

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # NFT-swap
 NFTSwap allow's you to trade NFT with other NFT on the same chain
+
+
+
+# Basic Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat accounts
+npx hardhat compile
+npx hardhat clean
+npx hardhat test
+npx hardhat node
+node scripts/sample-script.js
+npx hardhat help
+```
---
+++ contracts/main.sol
@@ -0,0 +1,73 @@
+//SPDX-License-Identifier: MIT
+//Developer: KryptoKoders
+//Github: https://github.com/KwikKodersIND/
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Main {
+    constructor() {}
+
+    uint256 counter = 0;
+
+    enum TradingObjectStatus {
+        Pending,
+        Confirmed,
+        Cancelled,
+        Expired,
+        WaitingForConfirmation1,
+        WaitingForConfirmation2
+    }
+
+    struct SwapTradingObject {
+        uint256 id;
+        address owner1;
+        address owner2;
+        uint256 token1;
+        uint256 token2;
+        address token1Address;
+        address token2Address;
+        bool owner1Confirmed;
+        bool owner2Confirmed;
+        string chainId;
+        TradingObjectStatus status;
+    }
+
+    struct OwnerTradingObject {
+        address owner;
+        uint256 tradingId;
+        bool isActive;
+    }
+
+    OwnerTradingObject public ownerTradingObject;
+    OwnerTradingObject[] public ownerTradingObjects;
+
+    mapping(address => OwnerTradingObject[]) public ownerTradingObjectsCount;
+
+    SwapTradingObject public swapTradingObject;
+    SwapTradingObject[] public swapTradingObjects;
+
+    mapping(uint256 => SwapTradingObject[]) public swapsAvailable;
+
+    function getID() internal returns (uint256) {
+        return ++counter;
+    }
+
+    function createTrade(
+        address caddress,
+        uint256 tokenID,
+        string calldata chainId
+    ) external payable {
+        uint256 id = getID();
+        ownerTradingObjects.push(OwnerTradingObject(msg.sender, id, true));
+        SwapTradingObject memory _swapTradingObj;
+        _swapTradingObj.id = id;
+        _swapTradingObj.owner1 = msg.sender;
+        _swapTradingObj.token1 = tokenID;
+        _swapTradingObj.token1Address = caddress;
+        _swapTradingObj.status = TradingObjectStatus.Pending;
+        _swapTradingObj.chainId = chainId;
+        swapTradingObjects.push(_swapTradingObj);
+    }
+}
