802a6dceaeeaf6dc0e5c82677a65693ea1a6e0b0
vijay ankith <vijay2ankith@gmail.com>
2022-05-13 14:21:26 +0530

    Update main.sol

--- contracts/main.sol
+++ contracts/main.sol
@@ -6,8 +6,9 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Main {
-    constructor() {}
+contract Main is ERC721 {
+
+    constructor() ERC721("NFTSwap", "NTSP") {}
 
     uint256 counter = 0;
 
